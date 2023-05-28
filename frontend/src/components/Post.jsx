e386ae00dcca60ba51b83a4f3ebacdcebd0f34a2
Your Name~ <you@example.com>
2023-05-29 00:36:03 +0530

    server hosted

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -6,7 +6,7 @@ const Post = ({ _id, title, summary, content, cover, createdAt }) => {
         <div className='post'>
             <Link to={`/post/${_id}`} >
                 <div className="image">
-                    <img src={`http://localhost:5000/${cover}`} alt="" />
+                    <img src={`https://backend-blogs-application.onrender.com/${cover}`} alt="" />
                 </div>
             </Link>
             <div className='texts'>
