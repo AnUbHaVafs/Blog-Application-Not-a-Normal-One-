73c83161078130d49f9eed13b7c6583140ea3526
Sophiya Singh <sophiya.singh2002@gmail.com>
2023-05-29 10:50:29 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const env = require('dotenv');
 const fs = require('fs');
 const app = express();
 const uploads = multer({ dest: 'uploads/' });
+const db = require('./config/db')
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -19,6 +20,11 @@ app.use('/api', require('./route/userRoutes'))
 app.use('/api', require('./route/postRoutes'))
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`to be started port : http://localhost:${PORT}`)
-});
+const start= async() => {
+    await db();
+    app.listen(process.env.PORT, () => {
+        console.log(`to be started port : http://localhost:${PORT}`)
+    });
+}
+
+start();
